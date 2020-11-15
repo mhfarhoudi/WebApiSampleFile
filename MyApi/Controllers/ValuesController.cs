a182bc65a957f713f2c46e8514e30a35f09183b1
Hossein Farhoudi <mh.farhoudi@outlook.com>
2020-11-15 13:03:16 +0330

    Add project files.

---
+++ MyApi/Controllers/ValuesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyApi.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value, IFormFile file)
+        {
+
+        }
+
+        [Route("Upload")]
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult Upload()
+        {
+            try
+            {
+                var json = Request.Form;
+                var file = json.Files[0];
+                var _products = Request.Form["JsonDetails"];
+               
+                var _pro = JsonConvert.DeserializeObject<Products>(_products);
+
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+                    var dbPath = Path.Combine(folderName, fileName);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private HttpContent byteArrayContent;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public BufferedSingleFileUploadDb FileUpload { get; set; }
+
+        public class BufferedSingleFileUploadDb
+        {
+            [Required]
+            [Display(Name = "File")]
+            public IFormFile FormFile { get; set; }
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async void OnPost()
+        {
+            HttpClient _client = new HttpClient();
+
+            var multipartContent = new MultipartFormDataContent();
+
+
+            Products products = new Products() { Id = 1, Name = "Hossein" };
+
+            var apiSerlize = JsonConvert.SerializeObject(products);
+
+            var content = new StringContent(apiSerlize, Encoding.UTF8, "application/json");
+
+            multipartContent.Add(content, "JsonDetails");
+
+            string _FileName = FileUpload.FormFile.FileName;
+
+            using (var fs = FileUpload.FormFile.OpenReadStream())
+            {
+                using (var streamContent = new StreamContent(fs))
+                {
+                    multipartContent.Add(streamContent, "files", _FileName);
+
+                    var postResponse = await _client.PostAsync($"http://localhost:52864/api/Values/Upload", multipartContent);
+
+                    if (postResponse.IsSuccessStatusCode)
+                    {
+                        await postResponse.Content.ReadAsStringAsync();
+                    }
+                    else
+                    {
+                        throw new WebException($"The remote server returned unexpcted status code: {postResponse.StatusCode} - {postResponse.ReasonPhrase}.");
+                    }
+                }
+            }
+
+
+        }
+    }
+}
